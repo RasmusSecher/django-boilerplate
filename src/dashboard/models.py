8cc5fddf397d4f2cdb0544c885b1601b62e6adec
rasmussecher <r.secher@live.dk>
2022-05-24 16:19:51 +0200

    Working prototype

--- src/dashboard/models.py
+++ src/dashboard/models.py
@@ -1,5 +1,22 @@
 from django.db import models
 
 
-class Visitor(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
+class Document(models.Model):
+    file = models.FileField()
+    executionTime = models.TextField(default="")
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.file.name}"
+
+
+class Attribute(models.Model):
+    document = models.ForeignKey(
+        Document, on_delete=models.CASCADE, related_name="attributes"
+    )
+
+    name = models.TextField(default="")
+    type = models.TextField()
+    regex = models.TextField()
+    security = models.IntegerField()
+    security_manual = models.BooleanField(default=False)
--- src/dashboard/urls.py
+++ src/dashboard/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import basicView
+from .views import basicView, dataView, downloadView
 
 urlpatterns = [
     path("", basicView),
+    path("data/", dataView),
+    path("download/", downloadView, name="download"),
 ]
